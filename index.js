484ad8fdf04d3e0a90b4f948f687662808e5f24c
MayamaTakeshi <mayamatakeshi@gmail.com>
2024-06-11 12:24:16 +0900

    moved ssml code to tone-stream

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
-const { ToneStream, utils } = require("tone-stream");
+const { ToneStream, utils, ssml } = require("tone-stream")
 
-const xml = require("xml-js");
+const xml = require("xml-js")
 
-const DEFAULT_DIGIT_DURATION = "50ms";
+const DEFAULT_DIGIT_DURATION = 50
+
+function gen_digits(sampleRate, text, duration) {
+  //console.log("gen_digits", text, duration)
+  if (text.match(/[^0-9a-fA-F\*\#]/)) {
+    throw `parse-failure: invalid DTMF sequence '${text}'`
+  }
+
+  const tones = utils.gen_dtmf_tones(
+    text,
+    duration,
+    duration,
+    sampleRate,
+  )
+  //console.log("tones", tones)
+  return tones
+}
 
 class DtmfGenerationStream extends ToneStream {
   constructor(opts) {
@@ -27,98 +43,8 @@ class DtmfGenerationStream extends ToneStream {
   }
 
   enqueue(params) {
-    var elements
-    if(typeof params == 'string') {
-      if(params.startsWith('<speak>')) {
-        const parsed = xml.xml2js(params);
-        elements = parsed.elements[0].elements;
-      } else {
-        elements = [
-          {
-            type: "text",
-            text: params
-          },
-        ];
-      }
-    } else {
-      if(params["content-type"] == "application/ssml+xml" || params.text.startsWith('<speak>')) {
-        const parsed = xml.xml2js(params.text)
-        elements = parsed.elements[0].elements
-      } else {
-        elements = [
-          {
-            type: "text",
-            text: params.text,
-          },
-        ];
-      }
-    }
-
-    //console.log(elements)
-
-    this.add([800, "s"]); // initial silence
-    this.process_elements(elements);
-    this.add([800, "s"]); // final silence
-  }
-
-  parse_duration(duration) {
-    if (duration.endsWith("ms")) {
-      return parseInt(duration);
-    } else if (duration.endsWith("s")) {
-      return parseInt(duration) * 1000;
-    } else {
-      throw `parse-failure: invalid duration ${duration}`;
-    }
-  }
-
-  push_silence(duration) {
-    var milliseconds = this.parse_duration(duration);
-    var samples = Math.round((this.format.sampleRate / 1000) * milliseconds);
-    this.add([samples, "s"]);
-  }
-
-  push_digits(digits, duration) {
-    var milliseconds = this.parse_duration(duration);
-
-    if (digits.match(/[^0-9a-fA-F\*\#]/)) {
-      throw `parse-failure: invalid DTMF sequence '${digits}'`;
-    }
-
-    this.concat(
-      utils.gen_dtmf_tones(
-        digits,
-        milliseconds,
-        milliseconds,
-        this.format.sampleRate,
-      ),
-    );
-  }
-
-  process_elements(elements) {
-    var res;
-    for (var i = 0; i < elements.length; i++) {
-      var e = elements[i];
-      if (e.type == "text") {
-        this.push_digits(e.text, DEFAULT_DIGIT_DURATION);
-      } else if (
-        e.type == "element" &&
-        e.name == "prosody" &&
-        e.attributes.rate &&
-        e.elements &&
-        e.elements[0] &&
-        e.elements[0].type == "text"
-      ) {
-        this.push_digits(e.elements[0].text, e.attributes.rate);
-      } else if (
-        e.type == "element" &&
-        e.name == "break" &&
-        typeof e.attributes.time == "string"
-      ) {
-        this.push_silence(e.attributes.time);
-      } else {
-        throw `parse-failure: invalid SSML element ${JSON.stringify(e)}`;
-      }
-    }
+    const tones = ssml.process(this.format.sampleRate, params.text, gen_digits, DEFAULT_DIGIT_DURATION)
+    this.concat(tones)
   }
 }
 
